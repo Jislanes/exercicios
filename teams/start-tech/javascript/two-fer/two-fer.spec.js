b6b3484128e6634218517c4e74b54771ca8d76cd
jislane.santos <jislane.santos@ambevtech.com.br>
2021-08-19 14:35:46 -0300

    Novo test

--- teams/start-tech/javascript/two-fer/two-fer.spec.js
+++ teams/start-tech/javascript/two-fer/two-fer.spec.js
@@ -8,8 +8,16 @@ describe('twoFer()', () => {
   test('a name given', () => {
     expect(twoFer('Alice')).toEqual('One for Alice, one for me.');
   });
+  test('empty name given', () => {
+    expect(twoFer('')).toEqual('One for you, one for me.');
+  });
+
+  test('null name given', () => {
+    expect(twoFer(null)).toEqual('One for you, one for me.');
+  });
 
   test('another name given', () => {
     expect(twoFer('Bob')).toEqual('One for Bob, one for me.');
   });
 });
+
