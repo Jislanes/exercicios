5043c9e8f012eefaea9f557804850c9e7cccc8ed
jislane.santos <jislane.santos@ambevtech.com.br>
2021-08-20 17:21:42 -0300

    JavaScript - Ex - triangle

---
+++ teams/start-tech/javascript/triangle/triangle.js
@@ -0,0 +1,46 @@
+//
+// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Triangle {
+  constructor(a,b,c) { //Lados do triângulo
+    this.a = a;
+    this.b = b;
+    this.c = c
+  }
+
+  get isEquilateral() {
+    if (this.a === this.b && this.a === this.c && this.b > 0 && this.c > 0 && this.b > 0) { // Garantindo que todos os lados são iguais e diferentes de zero
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  
+  get isIsosceles() {
+    if (this.a === this.b || this.a === this.c || this.c === this.b) { // Garantindo que pelo menos dois lados são iguais
+      if (this.b + this.c < this.a|| this.a+ this.c < this.b || this.b + this.a < this.c) { //Garantindo a desigualdade de triângulos
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  get isScalene() {
+    if (this.a !== this.b && this.b !== this.c && this.a !== this.c) { // Garantindo que todos os lados são diferentes
+      if (this.b + this.c < this.a|| this.a+ this.c < this.b || this.b + this.a < this.c) { //Garantindo a desigualdade de triângulos
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+}
+
