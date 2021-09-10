c61f7077f5c27587decbc9f0d85bda72e7c87ce0
jislane.santos <jislane.santos@ambevtech.com.br>
2021-09-10 13:47:28 -0300

    Resolvido

--- javascript/lucky-numbers/lucky-numbers.js
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -1,33 +1,32 @@
 // @ts-check
 
-/**
- * Calculates the sum of the two input arrays.
- *
- * @param {number[]} array1
- * @param {number[]} array2
- * @returns {number} sum of the two arrays
- */
 export function twoSum(array1, array2) {
-  throw new Error('Implement the twoSum function');
+  const soma1 = parseFloat(array1.join(''));
+  const soma2 = parseFloat(array2.join(''));
+    return  soma1 + soma2
 }
-
-/**
- * Checks whether a number is a palindrome.
- *
- * @param {number} value
- * @returns {boolean}  whether the number is a palindrome or not
- */
+    
 export function luckyNumber(value) {
-  throw new Error('Implement the luckyNumber function');
+  const string = String(value);
+  const array = string.split('')
+  .reverse()
+  .join('');
+  if (string == array){
+    return true
+  } else{
+    return false
+  }
 }
 
-/**
- * Determines the error message that should be shown to the user
- * for the given input value.
- *
- * @param {string|null|undefined} input
- * @returns {string} error message
- */
 export function errorMessage(input) {
-  throw new Error('Implement the errorMessage function');
+ let numero = Number(input);
+ console.log(numero)
+ if (input === '' || input === null || input === undefined){
+     return 'Required field';
+} else if (!numero){
+  return 'Must be a number besides 0';
+}
+else{
+  return ''
+}
 }
